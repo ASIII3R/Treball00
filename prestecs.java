b72e57a3686d9f2fedf1dfd94695ee30e0d7b2cc
Valeria <150950574+valeriasm2@users.noreply.github.com>
2025-01-09 18:03:22 +0100

    Desglose de préstecs

--- prestecs.java
+++ prestecs.java
@@ -1,21 +1,9 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Locale;
+
 
 public class prestecs {
 
     public static void main(String[] args) {
-
-        Locale.setDefault(Locale.US);
-
-        try {
-            String filePath = "./prestecs.json";
-            String content = new String(Files.readAllBytes(Paths.get(filePath)));
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        
 
     }
 }
