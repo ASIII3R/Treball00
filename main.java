ed0e0b75af362f0223d3ec998b62332dbba1fb26
ASIII3R <apozodorado.cf@iesesteveterradas.cat>
2025-01-09 16:27:15 +0100

    feat: Imprimir menú

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("gestió de biblioteca");
+        System.out.println("1. Llibres");
+        System.out.println("2. Usuaris");
+        System.out.println("3. Préstecs");
+        System.out.println("0. Sortir");
+        System.out.print("Escull una opció");
+
+        int opcion = scanner.nextInt();
+        
+    }
+}
