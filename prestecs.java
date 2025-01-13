03936c75020363649cffae54da5db0ef038f8ff6
Valeria <150950574+valeriasm2@users.noreply.github.com>
2025-01-13 15:52:09 +0100

    pruebas

--- prestecs.java
+++ prestecs.java
@@ -1,9 +1,35 @@
-
+import java.util.Scanner;
 
 public class prestecs {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String opciones;
         
+
+        System.out.println("Elige una opción:");
+        int opcs = sc.nextInt();
+
+        switch (opcs) {
+            case 1:
+                System.out.println("1. Afegir");
+                break;
+            case 2:
+                System.out.println("2. Modificar");
+                break;
+            case 3:
+                System.out.println("3. Llistes");
+                break;
+            case 4:
+                System.out.println("4. ELiminar");
+                break;
+            case 5:
+                System.out.println("5. Tornar enrrere.");
+                       
+
+        }
+
 
     }
 }
